e1e9adf751bae5412c3bc78402b0de9c1d9705b6
Emanuele <emanuelericci1998@libero.it>
2023-03-01 13:30:37 +0100

    Add name function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+if (args.Length == 0)
+{
+    Console.WriteLine("Hello, World");
+}
+else
+{
+    Console.WriteLine("Hello, " + args[0]);
+}
